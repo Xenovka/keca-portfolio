1c2a29365b4f2e299fe59d90d4063bba86a5e308
Xenovka <wendyyyanto@gmail.com>
2024-02-10 19:15:32 +0700

    Adding some text animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,69 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap/gsap-core";
+import { useRef } from "react";
+import SplitType from "split-type";
+
 function App() {
+    const container = useRef();
+
+    useGSAP(
+        () => {
+            // const tl = gsap.timeline({ defaults: { ease: "power3.in" } });
+            const name = SplitType.create(".name");
+            const role = SplitType.create(".role");
+            const tl = gsap.timeline({ defaults: { ease: "power4.out" } });
+
+            tl.fromTo(
+                name.chars,
+                {
+                    y: "100%",
+                    opacity: 0
+                },
+                {
+                    y: "0%",
+                    opacity: 1,
+                    duration: 1,
+                    stagger: 0.05
+                }
+            );
+
+            tl.fromTo(
+                role.chars,
+                {
+                    y: "100%",
+                    opacity: 0
+                },
+                {
+                    y: "0%",
+                    opacity: 1,
+                    duration: 1,
+                    stagger: 0.05
+                },
+                "<.75"
+            );
+        },
+        { scope: container }
+    );
+
     return (
-        <main>
-            <div className="grid grid-cols-12 mx-[60px] gap-5">
-                <h1 className="relative top-[30%] w-[1000px] h-80 leading-[0.6] text-[200px] col-start-1 col-span-9 font-bold">
-                    Keisha <span className="text-[140px]">Fidelia.</span>
-                </h1>
+        <main className="relative h-screen w-screen">
+            <div className="grid grid-cols-12 mx-[60px] gap-5" ref={container}>
+                <div className="absolute top-[20%] leading-[0.8] col-start-1 col-span-9">
+                    <div className="overflow-hidden">
+                        <span className="name text-[200px] font-bold">Keisha</span> <br />
+                    </div>
+                    <div className="overflow-hidden block">
+                        <span className="name text-[140px] font-bold">Fidelia.</span>
+                    </div>
+                </div>
                 <img
                     className="w-[1200px] h-[860px] bg-cover absolute bottom-0 left-0 -z-10 opacity-60"
                     alt="Keisha"
                     src="../src/assets/pretty.png"
                 />
-                <h2 className="relative top-[60%] col-start-5 col-span-4 w-[360px] font-light text-5xl">
-                    Freelance Designer
-                </h2>
+                <div className="absolute top-[55%] left-[35%] col-start-5 col-span-4 w-[360px] ">
+                    <span className="role font-light text-5xl">Freelance Designer</span>
+                </div>
             </div>
         </main>
     );
--- src/layouts/NavBar/index.jsx
+++ src/layouts/NavBar/index.jsx
@@ -1,13 +1,85 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap/gsap-core";
+import { useRef } from "react";
+import SplitType from "split-type";
+
 function NavBar() {
+    const container = useRef();
+
+    useGSAP(
+        () => {
+            const tl1 = gsap.timeline({ defaults: { ease: "power4.out" } });
+            const tl2 = gsap.timeline({ defaults: { ease: "power4.out" } });
+
+            const navItem = gsap.utils.toArray([".nav-works", ".nav-about"]);
+
+            navItem.forEach((item) => {
+                const splitNavWorks = SplitType.create(item.querySelectorAll(".nav-item-works"));
+                const splitNavWorksTop = SplitType.create(splitNavWorks.elements[0]);
+                const splitNavWorksBottom = SplitType.create(splitNavWorks.elements[1]);
+                const splitNavAbout = SplitType.create(item.querySelectorAll(".nav-item-about"));
+                const splitNavAboutTop = SplitType.create(splitNavAbout.elements[0]);
+                const splitNavAboutBottom = SplitType.create(splitNavAbout.elements[1]);
+
+                item.addEventListener("mouseenter", () => {
+                    if (item.classList.contains("nav-works")) {
+                        tl1.to(splitNavWorksBottom.chars, {
+                            y: "100%",
+                            duration: 0.5,
+                            stagger: 0.05
+                        }).to(
+                            splitNavWorksTop.chars,
+                            {
+                                y: "100%",
+                                duration: 0.5,
+                                stagger: 0.05
+                            },
+                            "<.25"
+                        );
+
+                        tl1.play();
+                    } else {
+                        tl2.to(splitNavAboutBottom.chars, {
+                            y: "100%",
+                            duration: 0.5,
+                            stagger: 0.05
+                        }).to(
+                            splitNavAboutTop.chars,
+                            {
+                                y: "100%",
+                                duration: 0.5,
+                                stagger: 0.05
+                            },
+                            "<.25"
+                        );
+
+                        tl2.play();
+                    }
+                });
+
+                item.addEventListener("mouseleave", () => {
+                    if (item.classList.contains("nav-works")) {
+                        tl1.reverse();
+                    } else {
+                        tl2.reverse();
+                    }
+                });
+            });
+        },
+        { scope: container }
+    );
+
     return (
-        <nav className="mt-9">
-            <ul className="navbar grid grid-cols-12 mx-[60px] gap-5">
-                <li className="nav-item col-span-2">Keisha Fidelia</li>
-                <li className="nav-item col-start-11 col-span-1 text-right">
-                    <a href="#">Works.</a>
+        <nav className="mt-9 absolute top-0 left-0 w-screen z-10" ref={container}>
+            <ul className="navbar grid grid-cols-12 mx-[60px] gap-5 overflow-hidden">
+                <li className="nav-title col-span-2">Keisha Fidelia</li>
+                <li className="relative nav-item nav-works w-fit col-start-11 col-span-1 cursor-pointer border-b-2">
+                    <span className="absolute -top-[100%] right-0 nav-item-works">Works.</span>
+                    <span className="nav-item-works">Works.</span>
                 </li>
-                <li className="nav-item col-start-12 col-span-1 text-right">
-                    <a href="#">About.</a>
+                <li className="relative nav-item nav-about w-fit col-start-12 col-span-1 cursor-pointer border-b-2">
+                    <span className="absolute -top-[100%] right-0 nav-item-about">About.</span>
+                    <span className="nav-item-about">About.</span>
                 </li>
             </ul>
         </nav>
